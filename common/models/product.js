f22f2c68f6199f2b06b51731cbe7e106c43bf54b
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-26 14:45:42 +0200

    06-egghead-define-a-remote-method-on-a-loopback-model

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(Product) {
-
+    const validQuantity = quantity => Boolean(quantity > 0);
+    Product.prototype.buy = function(quantity, callback) {
+        if (!validQuantity(quantity)) {
+            return callback(`Invalid quantity ${quantity}`);
+        }
+        var result = {quantity};
+        // TODO
+        callback(null, result);
+    };
 };
